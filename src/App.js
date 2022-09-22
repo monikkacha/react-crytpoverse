b7ea497866b6b5252d3d621194bd279c9a19b3af
monikkacha <monikkacha@gmail.com>
2022-09-22 12:31:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    Navbar,
+    Homepage,
+    Exchanges,
+    Cryptocurrencies,
+    CryptoDetail,
+    News
+} from './components/'
+
+const App = () => {
+    return (
+        <div className='App'>
+            <div className='navBar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path='/'>
+                                <Homepage />
+                            </Route>
+                        </Routes>
+                        <Routes>
+                            <Route path='/exchanges'>
+                                <Exchanges />
+                            </Route>
+                        </Routes>
+                        <Routes>
+                            <Route path='/cryptocurrencies'>
+                                <Cryptocurrencies />
+                            </Route>
+                        </Routes>
+                        <Routes>
+                            <Route path='/crypto/:coinId'>
+                                <CryptoDetail />
+                            </Route>
+                        </Routes>
+                        <Routes>
+                            <Route path='/news'>
+                                <News />
+                            </Route>
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
